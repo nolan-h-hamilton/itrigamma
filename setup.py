8fb7e0b3fe5071f850421dcf1c49504a31afce33
Nolan H. Hamilton <57746191+nolan-h-hamilton@users.noreply.github.com>
2026-02-14 22:02:43 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
-import sys
-from setuptools import Extension, setup
+from setuptools import Extension, setup, find_packages
 from Cython.Build import cythonize
 import numpy as np
-
 
 extensions = [
     Extension(
@@ -11,10 +9,7 @@ extensions = [
             "python/citrigamma.pyx",
             "src/itrigamma.c",
         ],
-        include_dirs=[
-            np.get_include(),
-            "src",
-        ],
+        include_dirs=[np.get_include(), "src"],
         language="c",
         define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
         extra_compile_args=["-O3"],
@@ -22,8 +17,11 @@ extensions = [
 ]
 
 setup(
+    packages=find_packages(),
     ext_modules=cythonize(
         extensions,
         compiler_directives={"language_level": "3"},
     ),
+    include_package_data=True,
+    zip_safe=False,
 )
